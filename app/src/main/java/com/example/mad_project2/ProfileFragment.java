e103468640f49a394be610c364c9e8192162e9a4
DivyaMulchandani <113537199+DivyaMulchandani@users.noreply.github.com>
2023-10-31 03:07:01 +0530

    profile backend

--- app/src/main/java/com/example/mad_project2/ProfileFragment.java
+++ app/src/main/java/com/example/mad_project2/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.mad_project2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,91 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.play.core.integrity.p;
+
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    TextView previousBooking , myRatings , Address , Members , Work , helpInput ;
+    ImageView profilePic;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+
+        previousBooking = view.findViewById(R.id.previous_booking);
+        myRatings = view.findViewById(R.id.my_ratings);
+        Address = view.findViewById(R.id.address);
+        Members = view.findViewById(R.id.members);
+        Work = view.findViewById(R.id.work);
+        helpInput = view.findViewById(R.id.help_input);
+
+        profilePic = view.findViewById(R.id.ellipse);
+
+        previousBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , previousBookingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myRatings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , myRatingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Members.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , MembershipActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Work.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity() , MembershipActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        helpInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity() , MembershipActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+
+
+        Address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , previousBookingActivity.class);
+                startActivity(intent);
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/mad_project2/myRatingActivity.java
@@ -0,0 +1,14 @@
+package com.example.mad_project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myRatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_rating2);
+    }
+}
